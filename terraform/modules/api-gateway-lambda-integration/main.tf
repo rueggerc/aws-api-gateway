bd5d22175f0c6a68a16d62a0ba225e2b445dfda2
Chris <chris.ruegger@gmail.com>
2019-11-03 05:09:57 -0500

    remote state

--- terraform/modules/api-gateway-lambda-integration/main.tf
+++ terraform/modules/api-gateway-lambda-integration/main.tf
@@ -34,10 +34,10 @@ resource "aws_api_gateway_integration" "request_method_integration" {
 
  # Permission
  resource "aws_lambda_permission" "allow_api_gateway" {
-     function_name = "${var.lambda_function_name}"
-     statement_id  = "AllowExecutionFromApiGateway"
-     action        = "lambda:InvokeFunction"
-     principal     = "apigateway.amazonaws.com"
-     source_arn    = "arn:aws:execute-api:${var.region}:${var.accountId}:${var.rest_api_id}/*/${aws_api_gateway_method.request_method.http_method}${aws_api_gateway_resource.resource.path}"
+    statement_id  = "AllowExecutionFromApiGateway"
+    action        = "lambda:InvokeFunction"
+    function_name = "${var.lambda_function_name}"
+    principal     = "apigateway.amazonaws.com"
+    source_arn    = "arn:aws:execute-api:${var.region}:${var.accountId}:${var.rest_api_id}/*/${aws_api_gateway_method.request_method.http_method}${aws_api_gateway_resource.resource.path}"
  }
 
